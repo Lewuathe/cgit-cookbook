2430f21708f1e89fd11e8deda8a580b4774e1c1f
lewuathe <lewuathe@me.com>
2015-06-17 23:02:13 +0900

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'lewuathe@me.com'
 license          'MIT'
 description      'Install and launching cgit server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/Lewuathe/cgit-cookbook'
+issues_url       'https://github.com/Lewuathe/cgit-cookbook/issues'
 version          '0.0.1'
 depends          'apache2'
 depends          'apt'
